4c2745b93f9a78973d3243c35bc5348e139e5bda
Stephen Wang <stephenwang1234567890@gmail.com>
2020-08-04 21:31:31 -0400

    deployment to heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ytdl = require("ytdl-core");
 const bot = new Discord.Client();
 const puppeteer = require('puppeteer');
 
-const token = process.env.token;
+const token = 'NzI3NjA4MzAzMTI2ODM5MzU2.XvuUCQ.yMDAybWk-RkApJPBYq9lp0YcAPo';
 const PREFIX = 'gimpbot ';
 const videos = {};
 
